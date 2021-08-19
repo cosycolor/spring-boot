c72bb7c7c2252e9a2d7d3d0815d736108d8155ff
LAPTOP-FN7HK094\Lenovo <handyoon87@gmail.com>
2021-08-19 23:21:59 +0900

    restful 구성

---
+++ src/main/java/com/example/mythymleaf/controller/BoardApiController.java
@@ -0,0 +1,57 @@
+package com.example.mythymleaf.controller;
+
+
+import com.example.mythymleaf.model.Board;
+import com.example.mythymleaf.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @GetMapping("/boards")
+    List<Board> all() {
+        return repository.findAll();
+    }
+    // end::get-aggregate-root[]
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+
+        return repository.findById(id).orElse(null);
+
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return repository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
